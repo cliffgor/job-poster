a3919b73eaface52ae0130e99808db0631895567
cliffgor <clierozigy@gmail.com>
2019-03-30 19:42:58 +0300

    finalised the appp by adding authentication and added a a constructor that displays the user who is logged in

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login'
+import './App.css';
 
+
+const config = {
+  issuer: 'https://dev-605422.okta.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oaej9cokVBAlQAHn356',
+  onAuthRequired:{onAuthRequired}
+}
+
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
+        <Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
+                  onAuthRequired={onAuthRequired}
+                  >
         <div className="App">
           <Navbar />
           <div className='container'>
           <Route path ="/" exact={true} component={Home} />
-          <Route path ="/staff" exact={true} component={Staff} />
+          <SecureRoute path ="/staff" exact={true} component={Staff} />
+          <Route path='/login' render={() =>  (
+          <Login baseUrl='https://dev-605422.okta.com'/> 
+          )} 
+          />
+          <Route path='/implicit/callback' component={ImplicitCallback} />
           </div>
         </div>
+        </Security>
       </Router>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <Link class="navbar-brand" to="/">Job Poster</Link> {/* Directs to the home component*/}
+            <Link class="navbar-brand" to="/">Job Poster</Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                 <Link class="nav-item nav-link active" to="/">Home <span class="sr-only">(current)</span></Link> {/* Directs to the home component*/}
-                <Link class="nav-item nav-link" to="/staff">Staff</Link> {/* Directs to the staff component*/}
-                <Link class="nav-item nav-link" href="#">Pricing</Link>
-                <Link class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Link>
+                <Link class="nav-item nav-link" to="/Login">Staff</Link> {/* Directs to the staff component*/}
+                {/* <Link class="nav-item nav-link" to="Login">Log In</Link> */}
+                {/* <Link class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Link> */}
     </div>
   </div>
 </nav>
